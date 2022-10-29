d1dda6dcecd761300a823b34c8f1180d0014e2fb
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-30 01:09:21 +0200

    Init the commandCooldownService and register all events

--- src/main/kotlin/net/onelitefeather/stardust/StardustPlugin.kt
+++ src/main/kotlin/net/onelitefeather/stardust/StardustPlugin.kt
@@ -6,10 +6,8 @@ import cloud.commandframework.paper.PaperCommandManager
 import net.onelitefeather.stardust.extenstions.buildCommandSystem
 import net.onelitefeather.stardust.extenstions.buildHelpSystem
 import net.onelitefeather.stardust.extenstions.initLuckPermsSupport
-import net.onelitefeather.stardust.service.DatabaseService
-import net.onelitefeather.stardust.service.I18nService
-import net.onelitefeather.stardust.service.LuckPermsService
-import net.onelitefeather.stardust.service.UserService
+import net.onelitefeather.stardust.listener.*
+import net.onelitefeather.stardust.service.*
 import org.bukkit.NamespacedKey
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
@@ -24,6 +22,7 @@ class StardustPlugin : JavaPlugin() {
     lateinit var signedNameSpacedKey: NamespacedKey
     lateinit var databaseService: DatabaseService
     lateinit var userService: UserService
+    lateinit var commandCooldownService: CommandCooldownService
     lateinit var luckPermsService: LuckPermsService
 
     override fun onEnable() {
@@ -38,6 +37,7 @@ class StardustPlugin : JavaPlugin() {
         )
 
         databaseService.init()
+        commandCooldownService = CommandCooldownService(this)
 
         initLuckPermsSupport()
         buildCommandSystem()
@@ -45,6 +45,12 @@ class StardustPlugin : JavaPlugin() {
 
         userService = UserService(this)
         userService.startUserTask()
+
+        server.pluginManager.registerEvents(CommandCooldownListener(this), this)
+        server.pluginManager.registerEvents(InventoryClickListener(this), this)
+        server.pluginManager.registerEvents(PlayerChatListener(this), this)
+        server.pluginManager.registerEvents(PlayerConnectionListener(this), this)
+        server.pluginManager.registerEvents(PlayerVanishListener(this), this)
 
         signedNameSpacedKey = NamespacedKey(this, "signed")
     }
