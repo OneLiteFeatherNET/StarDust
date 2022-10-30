bced67506c19721551c782b2a010c3db456f2da4
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-30 17:08:08 +0100

    Add dummy user object

--- src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
@@ -2,8 +2,12 @@ package net.onelitefeather.stardust.util;
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import net.onelitefeather.stardust.user.User
 import java.text.SimpleDateFormat
+import java.util.UUID
 
-val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
+
+const val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
+val DUMMY_USER = User(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
 val DATE_FORMAT = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
 val GSON: Gson = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().create()
