a35be38fc2a8b6dca28aa05c15496f0e84e4ea08
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-01 01:01:31 +0100

    Remove unnecessary gson field

--- src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
@@ -1,13 +1,9 @@
 package net.onelitefeather.stardust.util;
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import net.onelitefeather.stardust.user.User
+import net.onelitefeather.stardust.user.BukkitUser
 import java.text.SimpleDateFormat
 import java.util.UUID
 
-
 const val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
-val DUMMY_USER = User(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
+val DUMMY_Bukkit_USER = BukkitUser(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
 val DATE_FORMAT = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
-val GSON: Gson = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().create()
