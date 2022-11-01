6a48a4264b7a353acb2c0c8ab819e2801b1f5556
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-01 01:04:08 +0100

    Fix idea refactor issue

--- src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
@@ -5,5 +5,5 @@ import java.text.SimpleDateFormat
 import java.util.UUID
 
 const val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
-val DUMMY_Bukkit_USER = BukkitUser(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
+val DUMMY_USER = BukkitUser(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
 val DATE_FORMAT = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
