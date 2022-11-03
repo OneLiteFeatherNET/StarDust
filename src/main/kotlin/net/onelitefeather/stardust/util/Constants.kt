ebe5eb1243a052910316d2db77b2f4387ac879b6
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-03 23:26:45 +0100

    Rename BukkitUser to User and remove unnecessary constructor parameters

--- src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
@@ -1,9 +1,8 @@
 package net.onelitefeather.stardust.util;
 
-import net.onelitefeather.stardust.user.BukkitUser
+import net.onelitefeather.stardust.user.User
 import java.text.SimpleDateFormat
-import java.util.UUID
 
 const val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
-val DUMMY_USER = BukkitUser(-1, UUID.randomUUID().toString(), "Steve", vanished = false, flying = false)
+val DUMMY_USER = User(-1)
 val DATE_FORMAT = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
