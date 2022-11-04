1d984c75c2e8611cfff3dffb14f67cdcdef4d7fa
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-04 21:21:31 +0100

    Add properties call

--- src/main/kotlin/net/onelitefeather/stardust/listener/PlayerVanishListener.kt
+++ src/main/kotlin/net/onelitefeather/stardust/listener/PlayerVanishListener.kt
@@ -3,7 +3,7 @@ package net.onelitefeather.stardust.listener
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent
 import net.kyori.adventure.text.Component
 import net.onelitefeather.stardust.StardustPlugin
-import net.onelitefeather.stardust.api.user.User
+import net.onelitefeather.stardust.user.User
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -23,7 +23,7 @@ class PlayerVanishListener(private val stardustPlugin: StardustPlugin) : Listene
         val targetUser = stardustPlugin.userService.getUser(target.uniqueId)
 
         if (attacker is Permissible) {
-            event.isCancelled = if (targetUser != null && targetUser.isVanished()) {
+            event.isCancelled = if (targetUser != null && targetUser.properties.isVanished()) {
                 !attacker.hasPermission("stardust.bypass.damage.vanish")
             } else if (targetUser != null && target.isInvulnerable) {
                 !attacker.hasPermission("stardust.bypass.damage.invulnerable")
@@ -47,7 +47,7 @@ class PlayerVanishListener(private val stardustPlugin: StardustPlugin) : Listene
             entity = event.target
         }
 
-        if (user != null && user.isVanished() || entity != null && entity.isInvulnerable) {
+        if (user != null && user.properties.isVanished() || entity != null && entity.isInvulnerable) {
             event.isCancelled = true
         }
     }
@@ -56,7 +56,7 @@ class PlayerVanishListener(private val stardustPlugin: StardustPlugin) : Listene
     fun onFoodLevelChange(event: FoodLevelChangeEvent) {
         if (event.entity is Player) {
             val user = stardustPlugin.userService.getUser(event.entity.uniqueId)
-            event.isCancelled = user != null && (user.isVanished() || event.entity.isInvulnerable)
+            event.isCancelled = user != null && (user.properties.isVanished() || event.entity.isInvulnerable)
         }
     }
 
@@ -64,19 +64,19 @@ class PlayerVanishListener(private val stardustPlugin: StardustPlugin) : Listene
     fun onPickUp(event: EntityPickupItemEvent) {
         val livingEntity = event.entity
         val user = stardustPlugin.userService.getUser(livingEntity.uniqueId) ?: return
-        event.isCancelled = livingEntity is Player && user.isVanished()
+        event.isCancelled = livingEntity is Player && user.properties.isVanished()
     }
 
     @EventHandler
     fun onDrop(event: PlayerDropItemEvent) {
         val player = event.player
         val user = stardustPlugin.userService.getUser(player.uniqueId)
-        if (user != null && user.isVanished()) event.isCancelled = true
+        if (user != null && user.properties.isVanished()) event.isCancelled = true
     }
 
     @EventHandler
     fun onPlayerPickupExp(event: PlayerPickupExperienceEvent) {
-        event.isCancelled = stardustPlugin.userService.getUser(event.player.uniqueId)?.isVanished() == true
+        event.isCancelled = stardustPlugin.userService.getUser(event.player.uniqueId)?.properties?.isVanished() == true
     }
 
     @EventHandler
@@ -84,7 +84,7 @@ class PlayerVanishListener(private val stardustPlugin: StardustPlugin) : Listene
 
         val player = event.entity
         val user = stardustPlugin.userService.getUser(player.uniqueId) ?: return
-        if (user.isVanished()) {
+        if (user.properties.isVanished()) {
             event.drops.clear()
             event.deathMessage(Component.text(""))
             event.keepInventory = true
