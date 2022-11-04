52b76460971a4abb6be0a93f97c503720a4ef031
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-04 21:21:16 +0100

    Check if LuckPerms is enabled before set the player's display name

--- src/main/kotlin/net/onelitefeather/stardust/listener/PlayerConnectionListener.kt
+++ src/main/kotlin/net/onelitefeather/stardust/listener/PlayerConnectionListener.kt
@@ -2,6 +2,7 @@ package net.onelitefeather.stardust.listener
 
 import net.onelitefeather.stardust.StardustPlugin
 import net.onelitefeather.stardust.extenstions.miniMessage
+import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -14,7 +15,10 @@ class PlayerConnectionListener(private val stardustPlugin: StardustPlugin) : Lis
     fun handlePlayerJoin(event: PlayerJoinEvent) {
 
         val player = event.player
-        player.displayName(stardustPlugin.luckPermsService.getPlayerDisplayName(player))
+
+        if (stardustPlugin.luckPermsService.isEnabled()) {
+            player.displayName(stardustPlugin.luckPermsService.getPlayerDisplayName(player))
+        }
 
         val user = stardustPlugin.userService.getUser(player.uniqueId)
         if (user == null) {
@@ -24,24 +28,28 @@ class PlayerConnectionListener(private val stardustPlugin: StardustPlugin) : Lis
                 player.sendMessage(miniMessage {
                     stardustPlugin.i18nService.getMessage(
                         "plugin.first-join",
-                        stardustPlugin.i18nService.getPluginPrefix(),
-                        stardustPlugin.i18nService.translateLegacyString(player.displayName())
+                        *arrayOf(
+                            stardustPlugin.i18nService.getPluginPrefix(),
+                            stardustPlugin.i18nService.translateLegacyString(player.displayName())
+                        )
                     )
                 })
             }
             return
         }
 
-        user.checkCanFly()
+        player.allowFlight = user.properties.isFlying() && !player.allowFlight ||
+                player.gameMode == GameMode.CREATIVE || player.gameMode == GameMode.SPECTATOR
+
         if (!player.hasPermission("stardust.join.gamemode")) {
             player.gameMode = player.server.defaultGameMode
         }
 
         event.joinMessage(
-            if (user.isVanished()) null else miniMessage {
+            if (user.properties.isVanished()) null else miniMessage {
                 stardustPlugin.i18nService.getMessage(
                     "listener.join-message",
-                    stardustPlugin.i18nService.translateLegacyString(player.displayName())
+                    *arrayOf(stardustPlugin.i18nService.translateLegacyString(player.displayName()))
                 )
             }
         )
@@ -52,10 +60,10 @@ class PlayerConnectionListener(private val stardustPlugin: StardustPlugin) : Lis
         val player = event.player
         val user = stardustPlugin.userService.getUser(player.uniqueId)
         event.quitMessage(
-            if (user?.isVanished() == true) null else miniMessage {
+            if (user?.properties?.isVanished() == true) null else miniMessage {
                 stardustPlugin.i18nService.getMessage(
                     "listener.quit-message",
-                    stardustPlugin.i18nService.translateLegacyString(player.displayName())
+                    *arrayOf(stardustPlugin.i18nService.translateLegacyString(player.displayName()))
                 )
             }
         )
