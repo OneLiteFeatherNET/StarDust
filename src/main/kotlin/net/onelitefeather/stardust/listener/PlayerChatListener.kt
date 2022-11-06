d6eda74976ef68e800338e3eb9ab7d02d131a4a2
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-06 17:22:09 +0100

    Remove permission check

--- src/main/kotlin/net/onelitefeather/stardust/listener/PlayerChatListener.kt
+++ src/main/kotlin/net/onelitefeather/stardust/listener/PlayerChatListener.kt
@@ -4,7 +4,6 @@ import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 import net.onelitefeather.stardust.StardustPlugin
-import net.onelitefeather.stardust.extenstions.miniMessage
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -14,15 +13,11 @@ class PlayerChatListener(private val stardustPlugin: StardustPlugin) : Listener 
     @EventHandler
     fun onAsyncChat(event: AsyncChatEvent) {
         if (event.isCancelled) return
-        event.renderer { source: Player, sourceDisplayName: Component, _: Component, _: Audience ->
+        event.renderer { _: Player, sourceDisplayName: Component, _: Component, _: Audience ->
             Component.text()
                 .append(sourceDisplayName)
                 .append(Component.text(": "))
-                .append(
-                    if (source.hasPermission("chat.color")) miniMessage {
-                        stardustPlugin.i18nService.translateLegacyString(event.message())
-                    } else event.message()
-                )
+                .append(event.message())
                 .build()
         }
     }
