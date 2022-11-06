a80cc770f75762c59e1f42582db07c6b3eab0bdc
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-06 17:18:19 +0100

    Add constants for the heal command

--- src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/Constants.kt
@@ -4,5 +4,9 @@ import net.onelitefeather.stardust.user.User
 import java.text.SimpleDateFormat
 
 const val NOT_AVAILABLE_CONFIG_FALLBACK = "N/A (%s)"
+const val DEFAULT_PLAYER_FIRE_TICKS = 0
+const val DEFAULT_PLAYER_FOOD_LEVEL = 20
+const val DEFAULT_ENTITY_HAS_VISUAL_FIRE = false
+const val DEFAULT_PLAYER_SATURATION_LEVEL = 20.0F
 val DUMMY_USER = User(-1)
 val DATE_FORMAT = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
