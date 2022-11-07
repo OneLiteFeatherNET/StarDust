bb3e20c986e42bbbca833ab3902192c0e33f77e3
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-07 19:35:50 +0100

    Fix the file name to the correct format

--- src/main/kotlin/net/onelitefeather/stardust/service/I18nService.kt
+++ src/main/kotlin/net/onelitefeather/stardust/service/I18nService.kt
@@ -4,12 +4,13 @@ import net.kyori.adventure.util.UTF8ResourceBundleControl
 import net.onelitefeather.stardust.StardustPlugin
 import net.onelitefeather.stardust.util.NOT_AVAILABLE_CONFIG_FALLBACK
 import java.text.MessageFormat
+import java.util.Locale
 import java.util.ResourceBundle
 import kotlin.math.abs
 
 class I18nService(val stardustPlugin: StardustPlugin) {
 
-    val defaultMessages: ResourceBundle = ResourceBundle.getBundle("essentials", UTF8ResourceBundleControl())
+    val defaultMessages: ResourceBundle = ResourceBundle.getBundle("stardust", Locale.US, UTF8ResourceBundleControl())
 
     fun getPluginPrefix(): String {
         return MessageFormat.format(defaultMessages.getString("plugin.prefix"), stardustPlugin.name)
