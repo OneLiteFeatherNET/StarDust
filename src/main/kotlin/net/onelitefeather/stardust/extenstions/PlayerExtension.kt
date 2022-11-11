73267c1519b740e6650dc51e2f915d9483a00083
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-11 21:44:32 +0100

    Add method to remove current enemies from the player

--- src/main/kotlin/net/onelitefeather/stardust/extenstions/PlayerExtension.kt
+++ src/main/kotlin/net/onelitefeather/stardust/extenstions/PlayerExtension.kt
@@ -5,7 +5,19 @@ import io.sentry.protocol.Browser
 import io.sentry.protocol.User
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import org.bukkit.entity.Mob
 import org.bukkit.entity.Player
+
+fun Player.removeEnemies(radius: Double) {
+    this.location.getNearbyLivingEntities(radius).forEach { livingEntity ->
+        if (livingEntity is Mob) {
+            val target = livingEntity.target ?: return@forEach
+            if (target == this) {
+                livingEntity.target = null
+            }
+        }
+    }
+}
 
 fun Player.coloredDisplayName(): String = MiniMessage.miniMessage().serialize(
     LegacyComponentSerializer.legacyAmpersand()
