4819d3dacd0af32f57105b0432fdf9fbcc856c0e
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-15 23:15:03 +0100

    Add frogbucket command and remove unnecessary code

--- src/main/kotlin/net/onelitefeather/stardust/service/SyncFrogService.kt
+++ src/main/kotlin/net/onelitefeather/stardust/service/SyncFrogService.kt
@@ -1,28 +1,52 @@
 package net.onelitefeather.stardust.service
 
-import net.kyori.adventure.text.format.NamedTextColor
+import cloud.commandframework.annotations.Argument
+import cloud.commandframework.annotations.CommandDescription
+import cloud.commandframework.annotations.CommandMethod
+import cloud.commandframework.annotations.CommandPermission
+import cloud.commandframework.annotations.specifier.Quoted
+import io.papermc.paper.event.player.PrePlayerAttackEntityEvent
+import net.kyori.adventure.text.Component
 import net.onelitefeather.stardust.StardustPlugin
+import net.onelitefeather.stardust.extenstions.colorText
+import net.onelitefeather.stardust.extenstions.coloredDisplayName
 import net.onelitefeather.stardust.extenstions.convertComponentToString
 import net.onelitefeather.stardust.extenstions.miniMessage
-import org.bukkit.Location
-import org.bukkit.Material
-import org.bukkit.NamespacedKey
+import org.bukkit.*
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Frog
+import org.bukkit.entity.Frog.Variant
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 
-class SyncFrogService(stardustPlugin: StardustPlugin) : Listener {
+class SyncFrogService(val stardustPlugin: StardustPlugin) : Listener {
 
     private var frogNameSpacedKey: NamespacedKey = NamespacedKey(stardustPlugin, "frog_data_key")
+    private var frogVariantKey: NamespacedKey = NamespacedKey(stardustPlugin, "frog_variant_key")
+    private var frogCustomNameKey: NamespacedKey = NamespacedKey(stardustPlugin, "frog_custom_name_key")
+    private var frogInBucket: Int = 1
+    private var frogBucketName: String = "Frog Bucket"
 
     init {
         stardustPlugin.server.pluginManager.registerEvents(this, stardustPlugin)
+    }
+
+    @CommandMethod("frogbucket <customName> <variant> <amount>")
+    @CommandDescription("Gives you a Frog bucket with the given amount!")
+    @CommandPermission("stardust.command.frogbucket")
+    fun executeCommand(
+        player: Player,
+        @Quoted @Argument(value = "customName") customName: String,
+        @Argument(value = "variant") variant: Variant,
+        @Argument(value = "amount") amount: Int
+    ) {
+        addFrogBucketToPlayer(player, amount, miniMessage { customName }, variant, false)
     }
 
     @EventHandler
@@ -32,37 +56,57 @@ class SyncFrogService(stardustPlugin: StardustPlugin) : Listener {
         val itemStack = event.item ?: return
         val clickedBlock = event.clickedBlock ?: return
 
-        if(event.action != Action.RIGHT_CLICK_BLOCK) return
+        if (event.action != Action.RIGHT_CLICK_BLOCK) return
 
-        if (!itemStack.itemMeta.persistentDataContainer.has(frogNameSpacedKey)) return
+        if (!isFrogInBucket(itemStack)) return
         if (deserializeFrogData(itemStack, clickedBlock.location.add(0.0, 1.0, 0.0))) {
-            player.sendMessage(miniMessage { "Frog was successfully synced!" })
+
+            removeFrogBucket(player, player.inventory.itemInMainHand, true)
+
+            player.sendMessage(miniMessage {
+                stardustPlugin.i18nService.getMessage(
+                    "frog-bucket-spawn-success",
+                    *arrayOf(stardustPlugin.i18nService.getPluginPrefix())
+                )
+            })
         }
+    }
+
+    private fun removeFrogBucket(player: Player, itemStack: ItemStack, giveEmptyBucket: Boolean) {
+        if (player.gameMode == GameMode.CREATIVE) return
+        if (itemStack.amount > 1) {
+            player.inventory.setItemInMainHand(itemStack.subtract(1))
+        } else {
+            player.inventory.setItemInMainHand(null)
+        }
+
+        if (giveEmptyBucket) player.inventory.addItem(ItemStack(Material.BUCKET))
     }
 
     @EventHandler
-    fun handlePlayerEntityInteract(event: PlayerInteractEntityEvent) {
+    fun handlePlayerEntityInteract(event: PrePlayerAttackEntityEvent) {
 
         val player = event.player
-        val entity = event.rightClicked
+        val entity = event.attacked
+        val itemInHand = player.inventory.itemInMainHand
 
-        if (entity is Frog && player.inventory.itemInMainHand.type == Material.BUCKET) {
-            player.inventory.addItem(saveFrogData(entity, player))
+        if (isFrogInBucket(itemInHand)) return
+        if (entity is Frog && itemInHand.type == Material.BUCKET) {
+            addFrogBucketToPlayer(player, 1, entity.customName() ?: miniMessage { entity.name }, entity.variant, true)
             entity.health = 0.0
+            removeFrogBucket(player, itemInHand, false)
+            event.isCancelled = true
         }
     }
 
-    private fun saveFrogData(frog: Frog, player: Player): ItemStack {
-        val itemStack = player.inventory.itemInMainHand
-        val itemMeta = itemStack.itemMeta
+    private fun isFrogInBucket(itemStack: ItemStack): Boolean {
 
-        itemMeta.displayName(frog.customName() ?: miniMessage { "${frog.name}-Bucket" })
-        itemMeta.lore(listOf(miniMessage { NamedTextColor.GRAY.toString() + frog.variant.name.lowercase() }))
-
+        val itemMeta = itemStack.itemMeta ?: return false
         val container = itemMeta.persistentDataContainer
-        container[frogNameSpacedKey, PersistentDataType.STRING] = serializeFrogData(frog)
-        itemStack.itemMeta = itemMeta
-        return itemStack
+
+        if (!container.has(frogNameSpacedKey)) return false
+        val value = container[frogNameSpacedKey, PersistentDataType.INTEGER]
+        return value != null && value == frogInBucket
     }
 
     private fun deserializeFrogData(itemStack: ItemStack, location: Location): Boolean {
@@ -70,51 +114,83 @@ class SyncFrogService(stardustPlugin: StardustPlugin) : Listener {
         val frog = location.world.spawn(location, Frog::class.java)
         val itemMeta = itemStack.itemMeta
         val container = itemMeta.persistentDataContainer
-        if (!container.has(frogNameSpacedKey)) return false
+        if (!container.has(frogCustomNameKey) || !container.has(frogVariantKey)) return false
 
-        val rawData = container[frogNameSpacedKey, PersistentDataType.STRING] ?: return false
+        val customName = container[frogCustomNameKey, PersistentDataType.STRING]
+        val variant = container[frogVariantKey, PersistentDataType.STRING] ?: Variant.TEMPERATE.name
 
-        if (rawData.contains(":")) {
-            // Split the String by a colon
-            val data = rawData.split(":")
-
-            // Check the data contains a customName
-            if (data.contains("customName")) {
-
-                val customName = data[0].split("=")[0]
-                val variantName = data[1].split("=")[0]
-
-                frog.customName(miniMessage { customName })
-                frog.variant = Frog.Variant.valueOf(variantName)
-
-            } else {
-                if (data.contains("variant")) {
-                    val variantName = data[0].split("=")[0]
-                    frog.variant = Frog.Variant.valueOf(variantName)
-                }
-            }
-
-        } else {
-            //We can set the variant safely
-            frog.variant = Frog.Variant.valueOf(rawData.uppercase())
+        if (customName != null) {
+            frog.customName(miniMessage { customName.colorText() })
         }
 
+        frog.variant = Variant.valueOf(variant.uppercase())
         return true
     }
 
+    private fun createFrogItemStack(amount: Int, variant: Variant, customName: Component?): ItemStack {
 
-    private fun serializeFrogData(frog: Frog): String {
+        val itemStack = ItemStack(Material.BUCKET, amount)
+        val itemMeta = itemStack.itemMeta
 
-        var data = ""
-        val customName = frog.customName()
+        itemMeta.displayName(miniMessage { frogBucketName })
+
+        itemMeta.addEnchant(Enchantment.DIG_SPEED, 1, false)
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS)
+        itemMeta.lore(listOf(miniMessage { variant.name.lowercase() }))
+
+        val container = itemMeta.persistentDataContainer
 
         if (customName != null) {
-            data.plus("customName=").plus(convertComponentToString(customName)).plus(":").plus("variant=")
-                .plus(frog.variant.name)
-        } else {
-            data = frog.variant.name
+            container[frogCustomNameKey, PersistentDataType.STRING] = convertComponentToString(customName)
+        }
+        container[frogNameSpacedKey, PersistentDataType.INTEGER] = frogInBucket
+        container[frogVariantKey, PersistentDataType.STRING] = variant.name
+
+        itemStack.itemMeta = itemMeta
+        return itemStack
+    }
+
+    private fun addFrogBucketToPlayer(
+        player: Player,
+        amount: Int,
+        customName: Component,
+        variant: Variant,
+        actionBarMessage: Boolean
+    ) {
+
+        val slot = player.inventory.firstEmpty()
+
+        //If the slot returns -1 the inventory of the player is full
+        if (slot == -1) {
+            player.sendMessage(
+                miniMessage {
+                    stardustPlugin.i18nService.getMessage(
+                        "plugin.inventory-full",
+                        *arrayOf(stardustPlugin.i18nService.getPluginPrefix())
+                    )
+                }
+            )
+            return
         }
 
-        return data
+        val itemStack = createFrogItemStack(amount, variant, customName)
+        player.inventory.setItem(slot, itemStack)
+        val message = miniMessage {
+            stardustPlugin.i18nService.getMessage(
+                "frog-bucket-added-to-inventory",
+                *arrayOf(
+                    stardustPlugin.i18nService.getPluginPrefix(),
+                    amount,
+                    frogBucketName,
+                    player.coloredDisplayName()
+                )
+            )
+        }
+
+        if (!actionBarMessage) {
+            player.sendMessage(message)
+        } else {
+            player.sendActionBar(message)
+        }
     }
 }
