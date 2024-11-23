c01b7a4642a9303b0215443b081c99e025bbffbe
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-11-23 19:11:31 +0100

    Disable player vanish on demote

--- src/main/kotlin/net/onelitefeather/stardust/service/LuckPermsService.kt
+++ src/main/kotlin/net/onelitefeather/stardust/service/LuckPermsService.kt
@@ -1,11 +1,11 @@
 package net.onelitefeather.stardust.service
 
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
 import net.luckperms.api.LuckPerms
+import net.luckperms.api.event.EventSubscription
+import net.luckperms.api.event.user.track.UserDemoteEvent
 import net.luckperms.api.model.group.Group
-import net.luckperms.api.query.QueryOptions
 import net.onelitefeather.stardust.StardustPlugin
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.logging.Level
 
@@ -13,6 +13,8 @@ import java.util.logging.Level
 class LuckPermsService(val stardustPlugin: StardustPlugin) {
 
     lateinit var luckPerms: LuckPerms
+    private val luckPermsEvents: MutableList<EventSubscription<*>> = arrayListOf()
+
     fun init() {
         try {
             val server = stardustPlugin.server
@@ -21,23 +23,29 @@ class LuckPermsService(val stardustPlugin: StardustPlugin) {
                 if (provider != null) {
                     luckPerms = provider.provider
                     stardustPlugin.logger.log(Level.INFO, "Using ${provider.plugin.name} as Permission provider.")
+                    subscribeToEvents()
                 }
             }
         } catch (e: Exception) {
-            stardustPlugin.logger.throwing(LuckPermsService::class.java.simpleName, "init", e)
+            stardustPlugin.logger.log(Level.SEVERE, "Cannot hook into luckperms", e)
         }
     }
 
+    private fun subscribeToEvents() {
+        luckPermsEvents.add(luckPerms.eventBus.subscribe(UserDemoteEvent::class.java) { event ->
+
+            val player = Bukkit.getPlayer(event.user.uniqueId) ?: return@subscribe
+            if(!stardustPlugin.userService.playerVanishService.isVanished(player)) return@subscribe
+            stardustPlugin.userService.playerVanishService.toggle(player)
+        })
+    }
+
+    fun unsubscribeEvents() {
+        luckPermsEvents.forEach(EventSubscription<*>::close)
+        luckPermsEvents.clear()
+    }
+
     fun isEnabled(): Boolean = this::luckPerms.isInitialized
-
-    fun getPlayerDisplayName(player: Player): Component {
-        return MiniMessage.miniMessage().deserialize(getPlayerGroupPrefix(player).plus(" ${player.name}"))
-    }
-
-    fun getGroupPriority(groupName: String): Int {
-        val group = luckPerms.groupManager.getGroup(groupName) ?: return 0
-        return group.weight.orElse(0)
-    }
 
     fun getPrimaryGroup(player: Player): Group {
         val user = luckPerms.userManager.getUser(player.uniqueId) ?: return getDefaultGroup()
@@ -50,24 +58,4 @@ class LuckPermsService(val stardustPlugin: StardustPlugin) {
     }
 
     fun getDefaultGroup(): Group = luckPerms.groupManager.getGroup("default")!!
-
-    fun getPlayerGroupPrefix(player: Player): String {
-        val metaData = luckPerms.getPlayerAdapter(Player::class.java).getMetaData(player)
-        return metaData.prefix
-            ?: luckPerms.groupManager.getGroup("default")?.cachedData?.getMetaData(QueryOptions.nonContextual())?.prefix
-            ?: ""
-    }
-
-    fun getPlayerGroupSuffix(player: Player): String {
-        val metaData = luckPerms.getPlayerAdapter(Player::class.java).getMetaData(player)
-        return metaData.suffix
-            ?: luckPerms.groupManager.getGroup("default")?.cachedData?.getMetaData(QueryOptions.nonContextual())?.suffix
-            ?: ""
-    }
-
-    fun getGroupSortId(group: Group): Int {
-        return luckPerms.groupManager.loadedGroups.sortedByDescending {
-            getGroupPriority(it.name)
-        }.map { it.name }.indexOfFirst { it.equals(group.name, true) }
-    }
 }
