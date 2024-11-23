9c7c3d0ab785bf6c11591f764240286f38a81d76
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-11-23 19:02:31 +0100

    - Add method to check if the player can enter fly mode - Fix possible async catcher issues

--- src/main/kotlin/net/onelitefeather/stardust/util/PlayerUtils.kt
+++ src/main/kotlin/net/onelitefeather/stardust/util/PlayerUtils.kt
@@ -2,21 +2,29 @@ package net.onelitefeather.stardust.util
 
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import net.onelitefeather.stardust.StardustPlugin
+import org.bukkit.GameMode
 import org.bukkit.entity.Mob
 import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
 
 interface PlayerUtils {
+
+    fun canEnterFlyMode(player: Player) = player.gameMode == GameMode.CREATIVE || player.gameMode == GameMode.SPECTATOR
 
     /**
      * Remove the player as target for attack/look from the mobs in definied radius.
      * Prevents looks from entities to the player.
      */
     fun removeEnemies(player: Player, radius: Double) {
-        player.location.getNearbyLivingEntities(radius).forEach { livingEntity ->
-            if (livingEntity is Mob) {
-                val target = livingEntity.target ?: return@forEach
-                if (target == player) {
-                    livingEntity.target = null
+        val plugin = JavaPlugin.getPlugin(StardustPlugin::class.java)
+        player.server.scheduler.getMainThreadExecutor(plugin).execute {
+            player.location.getNearbyLivingEntities(radius).forEach { livingEntity ->
+                if (livingEntity is Mob) {
+                    val target = livingEntity.target ?: return@forEach
+                    if (target == player) {
+                        livingEntity.target = null
+                    }
                 }
             }
         }
