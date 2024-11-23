f0ff205b2bc5b867a471d1b150b5017ccd3aa918
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-11-23 19:12:28 +0100

    Add namespace key from main class

--- src/main/kotlin/net/onelitefeather/stardust/listener/PlayerChatListener.kt
+++ src/main/kotlin/net/onelitefeather/stardust/listener/PlayerChatListener.kt
@@ -3,25 +3,27 @@ package net.onelitefeather.stardust.listener
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
 import net.onelitefeather.stardust.StardustPlugin
-import net.onelitefeather.stardust.user.UserPropertyType
+import org.bukkit.NamespacedKey
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class PlayerChatListener(private val stardustPlugin: StardustPlugin) : Listener {
+
+    private val chatConfirmationKey = NamespacedKey(stardustPlugin, "chat_confirmation")
 
     @EventHandler
     fun onAsyncChat(event: AsyncChatEvent) {
         val player = event.player
         val user = stardustPlugin.userService.getUser(player.uniqueId)
 
-        if (user != null && user.properties.getProperty(UserPropertyType.VANISHED).getValue<Boolean>() == true) {
+        if (user != null && user.isVanished()) {
 
-            if (!user.hasChatConfirmation(stardustPlugin.chatConfirmationKey)) {
-                user.confirmChatMessage(stardustPlugin.chatConfirmationKey, true)
+            if (!user.hasChatConfirmation(chatConfirmationKey)) {
+                user.confirmChatMessage(chatConfirmationKey, true)
                 event.isCancelled = true
                 player.sendMessage(Component.translatable("vanish.confirm-chat-message").arguments(stardustPlugin.getPluginPrefix()))
             } else {
-                user.confirmChatMessage(stardustPlugin.chatConfirmationKey, false)
+                user.confirmChatMessage(chatConfirmationKey, false)
             }
 
             return
