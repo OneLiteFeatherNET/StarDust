510b40b52bf4f59832ea9f43b492cc359cb68ed5
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-11-23 19:09:28 +0100

    Add messages for the target player

--- src/main/kotlin/net/onelitefeather/stardust/command/commands/GodmodeCommand.kt
+++ src/main/kotlin/net/onelitefeather/stardust/command/commands/GodmodeCommand.kt
@@ -1,21 +1,21 @@
 package net.onelitefeather.stardust.command.commands
 
-import cloud.commandframework.annotations.Argument
-import cloud.commandframework.annotations.CommandDescription
-import cloud.commandframework.annotations.CommandMethod
-import cloud.commandframework.annotations.CommandPermission
-import cloud.commandframework.annotations.specifier.Greedy
 import net.kyori.adventure.text.Component
 import net.onelitefeather.stardust.StardustPlugin
 import net.onelitefeather.stardust.util.PlayerUtils
 import net.onelitefeather.stardust.util.RADIUS_REMOVE_ENEMIES
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.incendo.cloud.annotation.specifier.Greedy
+import org.incendo.cloud.annotations.Argument
+import org.incendo.cloud.annotations.Command
+import org.incendo.cloud.annotations.CommandDescription
+import org.incendo.cloud.annotations.Permission
 
 class GodmodeCommand(private val stardustPlugin: StardustPlugin) : PlayerUtils {
 
-    @CommandMethod("godmode [player]")
-    @CommandPermission("stardust.command.godmode")
+    @Command("godmode [player]")
+    @Permission("stardust.command.godmode")
     @CommandDescription("Makes a player invulnerable to everything")
     fun handleCommand(commandSender: CommandSender, @Greedy @Argument(value = "player") target: Player?) {
         handleInvulnerability(commandSender, target ?: commandSender as Player)
@@ -32,12 +32,17 @@ class GodmodeCommand(private val stardustPlugin: StardustPlugin) : PlayerUtils {
             target.isInvulnerable = !target.isInvulnerable
             removeEnemies(target, RADIUS_REMOVE_ENEMIES)
 
-            val enabledMessage = Component.translatable("commands.god-mode.enable").arguments(stardustPlugin.getPluginPrefix(), target.displayName())
-            val disabledMessage = Component.translatable("commands.god-mode.disable").arguments(stardustPlugin.getPluginPrefix(), target.displayName())
+            val targetEnabledMessage = Component.translatable("commands.godmode.enable.target").arguments(stardustPlugin.getPluginPrefix())
+            val targetDisabledMessage = Component.translatable("commands.godmode.disable.target").arguments(stardustPlugin.getPluginPrefix())
 
-            target.sendMessage(if (target.isInvulnerable) enabledMessage else disabledMessage)
-            if (commandSender != target) {
+            val enabledMessage = Component.translatable("commands.godmode.enable").arguments(stardustPlugin.getPluginPrefix(), target.displayName())
+            val disabledMessage = Component.translatable("commands.godmode.disable").arguments(stardustPlugin.getPluginPrefix(), target.displayName())
+
+            if(commandSender == target) {
+                target.sendMessage(if (target.isInvulnerable) targetEnabledMessage else targetDisabledMessage)
+            } else {
                 commandSender.sendMessage(if (target.isInvulnerable) enabledMessage else disabledMessage)
+                target.sendMessage(if (target.isInvulnerable) targetEnabledMessage else targetDisabledMessage)
             }
         } catch (e: Exception) {
             this.stardustPlugin.logger.throwing(GodmodeCommand::class.java.simpleName, "handleInvulnerability", e)
