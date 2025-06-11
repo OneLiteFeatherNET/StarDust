1a1d63048aa25787aa4c6b9b74cdeb856897eed3
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-06-11 22:48:28 +0200

    Rewrite the Kotlin class into a Java class

--- src/main/java/net/onelitefeather/stardust/api/ItemSignService.java
+++ src/main/java/net/onelitefeather/stardust/api/ItemSignService.java
@@ -1,14 +1,12 @@
-package net.onelitefeather.stardust.api
+package net.onelitefeather.stardust.api;
 
-import net.kyori.adventure.text.Component
-import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemStack
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-/**
- * API Class to handle item Signing
- * @author TheMeinerLP
- */
-interface ItemSignService<I : ItemStack, P : Player> {
+import java.util.List;
+
+public interface ItemSignService<I extends ItemStack, P extends Player> {
 
     /**
      * Sign an item with specific lore lines and more
@@ -16,20 +14,19 @@ interface ItemSignService<I : ItemStack, P : Player> {
      * @param lore to bet set to the item
      * @param player be used for replacement on the lore
      */
-    fun sign(baseItemStack: I, lore: List<Component>, player: P): I
+    I sign(I baseItemStack, List<Component> lore, P player);
 
     /**
      * Remove a signature from an item
      * @param baseItemStack to be modified
      * @param player be used for replacement on the lore
      */
-    fun removeSignature(baseItemStack: I, player: P): I
+    I removeSignature(I baseItemStack, P player);
 
     /**
      * Checks if the item be signed
      * @param itemStack to be checked
      * @param player be used
      */
-    fun hasSigned(itemStack: I, player: P): Boolean
-
+    boolean hasSigned(I itemStack, P player);
 }
