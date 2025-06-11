8ed7a907bb492dcce78b4355e23cde2bb5770a53
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-06-11 22:49:11 +0200

    Rewrite the Kotlin class into a Java class

--- src/main/java/net/onelitefeather/stardust/api/PlayerVanishService.java
+++ src/main/java/net/onelitefeather/stardust/api/PlayerVanishService.java
@@ -1,56 +1,52 @@
-package net.onelitefeather.stardust.api
+package net.onelitefeather.stardust.api;
 
-import org.bukkit.entity.Player
+import org.bukkit.entity.Player;
 
-/**
- * API Class to handle player vanishing
- * @author TheMeinerLP
- */
-interface PlayerVanishService<P : Player> {
+public interface PlayerVanishService<P extends Player> {
 
     /**
      * Hides a player for other players
      * @param player to be hide
      */
-    fun hidePlayer(player: P)
+    void hidePlayer(P player);
 
     /**
      * Shows a player for other players
      * @param player to be shown to other player
      */
-    fun showPlayer(player: P)
+    void showPlayer(P player);
 
     /**
      * Toggle vanish status of a player
      * @param player be affected
      */
-    fun toggle(player: P): Boolean
+    boolean toggle(P player);
 
     /**
      * Check if the player in vanish
      * @return the current status
      */
-    fun isVanished(player: P): Boolean
+    boolean isVanished(P player);
 
     /**
      * Set a user explicit the vanish status
-     * @param vanished status of the user
      * @param player to be affected
+     * @param vanished status of the user
      */
-    fun setVanished(player: P, vanished: Boolean)
+    void setVanished(P player, boolean vanished);
 
     /**
      * Handles if a player joining the server
      * @param player be affected
      * @return true if the player is vanished
      */
-    fun handlePlayerJoin(player: P): Boolean
+    boolean handlePlayerJoin(P player);
 
     /**
      * Handles if a player quits the server
      * @param player be affected
      */
-    fun handlePlayerQuit(player: P)
+    void handlePlayerQuit(P player);
 
-    fun canSee(player: P, target: P): Boolean
+    boolean canSee(P player, P target);
 }
